625c44cfe420515e1dfad10766c0d2ea276b09b1
phantom5820 <phantom5820@naver.com>
2021-03-12 19:45:06 +0900

    2진수 변환 기본 틀

---
+++ binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+void binary1(int n){
+
+
+}
+
+void binary2(int n){
+
+}
+
+
+int main(void){
+	int num;
+	printf("정수입력 : ");
+	scanf("%d\n",&num);
+	binary1(num);
+	return 0;
+}
